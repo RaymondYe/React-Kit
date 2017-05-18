5cb3d833eaf5bbc07a6bd70d6bff127e8974a53c
raymondYip <raymondyip1022@gmail.com>
2017-05-18 16:51:33 +0800

    remove unuse file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-import polyfill from 'babel-polyfill';
 import React from 'react';
 import ReactDom from 'react-dom';
-import APP from './components/app';
+import APP from './containers/app';
 
 const AppElement = document.getElementById('app');
 
 // Render App Components
 ReactDom.render(
-  <APP/>,
-  AppElement
+	<APP/>,
+	AppElement
 );
---
+++ src/containers/app/app.jsx
@@ -0,0 +1,16 @@
+import React, { PropTypes, Component } from 'react';
+
+import '../../styles/common.less';
+import './app.less';
+
+class App extends Component{
+	render() {
+		return (
+			<div className="app">
+				React-Kit
+			</div>
+		);
+	}
+}
+
+export default App
